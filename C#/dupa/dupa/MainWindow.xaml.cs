90a4d4afa00a2800476e2974bb1da69cd7be299e
micjaw310589 <mj310589@student.polsl.pl>
2025-03-16 02:22:32 +0100

    zad2

---
+++ C#/dupa/dupa/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void DrawLine(int x1, int x2, int y1, int y2, int thickness, Brush color)
+        {
+            var line = new Line();
+            line.Stroke = color;
+            line.X1 = x1;
+            line.X2 = x2;
+            line.Y1 = y1;
+            line.Y2 = y2;
+            line.HorizontalAlignment = HorizontalAlignment.Left;
+            line.VerticalAlignment = VerticalAlignment.Center;
+            line.StrokeThickness = thickness;
+
+            canvas.Children.Add(line);
+        }
+        private void DrawStar(int x, int y, int width, int thicknes, int count, int depth = 0)
+        { 
+            for (int i = 0; i < count; i++)
+            {
+                double angle = (2 * Math.PI / count) * i;
+
+                int x1 = x + (int)((width) * Math.Cos(angle));
+                int x2 = x - (int)((width) * Math.Cos(angle));
+
+                int y1 = y + (int)((width) * Math.Sin(angle));
+                int y2 = y - (int)((width) * Math.Sin(angle));
+
+                DrawLine(x1, x2, y1, y2, thicknes, Brushes.Black);
+
+                if (depth > 0)
+                {
+                    DrawStar(x1, y1, width / 3, thicknes, count, depth - 1);
+                    DrawStar(x2, y2, width / 3, thicknes, count, depth - 1);
+                }
+            }
+        }
+
+        int depth = 0;
+        int count = 1;
+        int width = 100;
+
+        int centerX = 0;
+        int centerY = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            centerX = System.Convert.ToInt32((canvas.ActualWidth) / 2);
+            centerY = System.Convert.ToInt32((canvas.ActualHeight) / 2);
+
+            UpdateCanvas();
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            centerX = System.Convert.ToInt32((canvas.ActualWidth) / 2);
+            centerY = System.Convert.ToInt32((canvas.ActualHeight) / 2);
+
+            UpdateCanvas();
+        }
+
+        private void UpdateCanvas()
+        {
+            canvas.Children.Clear();
+            centerX = System.Convert.ToInt32((canvas.ActualWidth) / 2);
+            centerY = System.Convert.ToInt32((canvas.ActualHeight) / 2);
+
+            DrawStar(centerX, centerY, width * 10, 1, count, depth);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            depth = (int)e.NewValue;
+            UpdateCanvas();
+        }
+
+        private void Depth(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            count = (int)e.NewValue;
+            UpdateCanvas();
+        }
+
+        private void Widthset(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            width = (int)e.NewValue;
+            UpdateCanvas();
+        }
+    }
+}
--- C#/zad2/zad2/MainWindow.xaml.cs
+++ C#/zad2/zad2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Runtime.Intrinsics.Arm;
+using System.Text; 
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,68 +23,226 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    void Gwiazda(int r, int sr_x, int sr_y, int min_r)
+    void RysujLinie(int x1, int y1, int x2, int y2, SolidColorBrush kolor)
     {
-        Line l1 = new();
-        Line l2 = new();
-        Line l3 = new();
-        Line l4 = new();
-        Line l5 = new();
-        Line l6 = new();
+        Line l = new();
+        l.X1 = x1;
+        l.Y1 = y1;
+        l.X2 = x2;
+        l.Y2 = y2;
+        l.Stroke = kolor;
 
-        l1.Stroke = Brushes.Blue;
-        l2.Stroke = Brushes.Blue;
-        l3.Stroke = Brushes.Blue;
-        l4.Stroke = Brushes.Blue;
-        l5.Stroke = Brushes.Blue;
-        l6.Stroke = Brushes.Blue;
+        cv.Children.Add(l);
+    }
 
-        l1.X1 = sr_x;
-        l1.Y1 = sr_y;
-        l2.X1 = sr_x;
-        l2.Y1 = sr_y;
-        l3.X1 = sr_x;
-        l3.Y1 = sr_y;
-        l4.X1 = sr_x;
-        l4.Y1 = sr_y;
-        l5.X1 = sr_x;
-        l5.Y1 = sr_y;
-        l6.X1 = sr_x;
-        l6.Y1 = sr_y;
+    void Gwiazda(int r, int sr_x, int sr_y, int min_r, SolidColorBrush kolor)
+    {
+        double alfa;
+        int x2,
+            y2;
 
-        l1.X2 = sr_x + r;
-        l1.Y2 = sr_y;
-        l2.X2 = sr_x + r / 2;
-        l2.Y2 = sr_y + Math.Sqrt(Math.Pow(r, 2) - Math.Pow(r / 2, 2));
-        l3.X2 = sr_x - r / 2;
-        l3.Y2 = sr_y + Math.Sqrt(Math.Pow(r, 2) - Math.Pow(r / 2, 2));
-        l4.X2 = sr_x - r;
-        l4.Y2 = sr_y;
-        l5.X2 = sr_x - r / 2;
-        l5.Y2 = sr_y - Math.Sqrt(Math.Pow(r, 2) - Math.Pow(r / 2, 2));
-        l6.X2 = sr_x + r / 2;
-        l6.Y2 = sr_y - Math.Sqrt(Math.Pow(r, 2) - Math.Pow(r / 2, 2));
-
-        cv.Children.Add(l1);
-        cv.Children.Add(l2);
-        cv.Children.Add(l3);
-        cv.Children.Add(l4);
-        cv.Children.Add(l5);
-        cv.Children.Add(l6);
-
-        if (r >= min_r)
+        for (int i = 0; i < 6; i++)
         {
-            Gwiazda(r / 3, Convert.ToInt16(l1.X2), Convert.ToInt16(l1.Y2), min_r);
-            Gwiazda(r / 3, Convert.ToInt16(l2.X2), Convert.ToInt16(l2.Y2), min_r);
-            Gwiazda(r / 3, Convert.ToInt16(l3.X2), Convert.ToInt16(l3.Y2), min_r);
-            Gwiazda(r / 3, Convert.ToInt16(l4.X2), Convert.ToInt16(l4.Y2), min_r);
-            Gwiazda(r / 3, Convert.ToInt16(l5.X2), Convert.ToInt16(l5.Y2), min_r);
-            Gwiazda(r / 3, Convert.ToInt16(l6.X2), Convert.ToInt16(l6.Y2), min_r);
+            alfa = 2 * Math.PI / 6 * i;
+            x2 = sr_x - Convert.ToInt32(r * Math.Cos(alfa));
+            y2 = sr_y + Convert.ToInt32(r * Math.Sin(alfa));
+
+            RysujLinie(sr_x, sr_y, x2, y2, kolor);
+
+            if (r >= min_r)
+            {
+                Gwiazda(r / 3, x2, y2, min_r, kolor);
+            }
         }
+    }
+
+    void Zamien(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void WypelnijTablice2D(double lower, double higher, ref double[,] arr2D)
+    {
+        Random rng = new Random();
+
+        for (int i = 0; i < arr2D.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr2D.GetLength(1); j++)
+            {
+                arr2D[i, j] = rng.NextDouble() * (higher - lower) + lower;
+            }
+        }
+    }
+
+    void WyswietlListBox(ref ListBox listB, in double[,] arr2D)
+    {
+        ListBoxItem row;
+
+        for (int i = 0; i < arr2D.GetLength(0); i++)
+        {
+            row = new();
+            for (int j = 0; j < arr2D.GetLength(1); j++)
+            {
+                row.Content += $"{arr2D[i, j]:###.#}\t";
+            }
+            row.Content += $"[{SumaWiersza(arr2D, i):###.#}]\t";
+            listB.Items.Add(row);
+        }
+
+        row = new();
+        for (int j = 0; j < arr2D.GetLength(1); j++)
+        {
+            row.Content += $"[{SumaKolumny(arr2D, j):###.#}]\t";
+        }
+        listB.Items.Add(row);
+    }
+
+    void WyswietlListBox(ref ListBox listB, in int[][] arr2D)
+    {
+        ListBoxItem row;
+
+        for (int i = 0; i < arr2D.Length; i++)
+        {
+            row = new();
+            for (int j = 0; j < arr2D[i].GetLength(0); j++)
+            {
+                row.Content += $"{arr2D[i][j]:###.#}\t";
+            }
+            row.Content += $"[{SumaWiersza(arr2D, i):###.#}]\t";
+            listB.Items.Add(row);
+        }
+    }
+
+    private double SumaWiersza(in double[,] arr2D, in int row)
+    {
+        if (row <= arr2D.GetLength(0))
+        {
+            double suma = 0.0;
+
+            for (int i = 0; i < arr2D.GetLength(1); i++)
+            {
+                suma += arr2D[row, i];
+            }
+
+            return suma;
+        }
+        else
+            throw new IndexOutOfRangeException();
+    }
+
+    private double SumaWiersza(in int[][] arr2D, in int row)
+    {
+        if (row <= arr2D.GetLength(0))
+        {
+            double suma = 0.0;
+
+            for (int i = 0; i < arr2D[row].GetLength(0); i++)
+            {
+                suma += arr2D[row][i];
+            }
+
+            return suma;
+        }
+        else
+            throw new IndexOutOfRangeException();
+    }
+
+    private double SumaKolumny(in double[,] arr2D, in int col)
+    {
+        if (col <= arr2D.GetLength(1))
+        {
+            double suma = 0.0;
+
+            for (int i = 0; i < arr2D.GetLength(0); i++)
+            {
+                suma += arr2D[i, col];
+            }
+
+            return suma;
+        }
+        else
+            throw new IndexOutOfRangeException();
     }
 
     private void btnRysuj_Click(object sender, RoutedEventArgs e)
     {
-        Gwiazda(200, 350, 350, 10);
+        cv.Children.Clear();
+
+        try
+        {
+            int r = Convert.ToInt32(txtR.Text),
+                min_r = Convert.ToInt32(txtMin.Text),
+                x = Convert.ToInt32(txtX.Text),
+                y = Convert.ToInt32(txtY.Text);
+
+            if (r < 0 || min_r <= 0 || x < 0 || y < 0)
+            {
+                MessageBox.Show("Złe dane wejściowe");
+            }
+            else
+            {
+                Gwiazda(
+                    Convert.ToInt32(txtR.Text),
+                    Convert.ToInt32(txtX.Text),
+                    Convert.ToInt32(txtY.Text),
+                    Convert.ToInt32(txtMin.Text),
+                    Brushes.Blue
+                    );
+            }
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Wprowadź liczby!");
+        }
+    }
+
+    private void btnTest2_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            int A = Convert.ToInt32(txtA.Text),
+                B = Convert.ToInt32(txtB.Text);
+
+            Zamien(ref A, ref B);
+
+            MessageBox.Show($"A = {A}\nB = {B}");
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show($"Wprowadź liczby!");
+        }
+    }
+
+    private void btnWyswietl2DArr_Click(object sender, RoutedEventArgs e)
+    {
+        int SIZE_COL = 3,
+            SIZE_ROW = 5;
+
+        double[,] arr2D = new double[SIZE_ROW, SIZE_COL];
+        WypelnijTablice2D(0.0, 100.0, ref arr2D);
+        WyswietlListBox(ref listTab, arr2D);
+    }
+
+    private void btnWyswietlJagged_Click(object sender, RoutedEventArgs e)
+    {
+        int SIZE_ROW = 3,
+            SIZE_COL = 8;
+
+        int[][] arrJagged = new int[SIZE_ROW][];
+
+        for (int i = 0; i < SIZE_ROW; i++)
+        {
+            int[] row = new int[SIZE_COL - 1];
+            for (int j = 2; j < SIZE_COL + 1; j++)
+            {
+                row[j - 2] = j;
+            }
+            arrJagged[i] = row;
+            SIZE_COL -= 2;
+        }
+
+        WyswietlListBox(ref listTab, arrJagged);
     }
 }
