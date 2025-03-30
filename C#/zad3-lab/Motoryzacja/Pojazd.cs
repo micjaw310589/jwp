238706856eea6e5595196cf7f339b716b1a1bf86
micjaw310589 <mj310589@student.polsl.pl>
2025-03-31 01:14:53 +0200

    zadania lab 3 (domowe niedokończone)

---
+++ C#/zad3-lab/Motoryzacja/Pojazd.cs
@@ -0,0 +1,172 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Motoryzacja
+{
+    struct RejestrNazw
+    {
+        public DateTime DataModyfikacji;
+        public string Nazwa;
+
+        public override string ToString()
+        {
+            return $"{this.DataModyfikacji}\t {this.Nazwa}";
+        }
+
+        public RejestrNazw(DateTime data, string nazwa)
+        {
+            this.DataModyfikacji = data;
+            this.Nazwa = nazwa;
+        }
+    }
+
+    public class Pojazd
+    {
+        private string nazwa = "";
+        private byte liczbakol;
+        private double predkosc;
+        private static int liczbaPojazdow;
+
+        public int Lp { get; private set; }
+
+        private List<RejestrNazw> HistoriaNazw { get; set; } = new List<RejestrNazw>();
+
+        public string Nazwa
+        {
+            get => this.nazwa;
+            set
+            {
+                HistoriaNazw.Add(new RejestrNazw(DateTime.Now, value));
+                this.nazwa = value;
+            }
+        }
+
+        public byte LiczbaKol
+        {
+            get { return this.liczbakol; }
+            set
+            {
+                this.liczbakol = value;
+            }
+        }
+
+        public double Predkosc
+        {
+            get { return this.predkosc; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Nieprawidłowa prędkość");
+                else
+                    this.predkosc = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Lp}/{liczbaPojazdow}\t {this.Nazwa}\t V-Max: {this.Predkosc}\t Koła: {this.LiczbaKol}";
+        }
+
+        static Pojazd()
+        {
+            liczbaPojazdow = 0;
+        }
+
+        public Pojazd()
+        {
+            Lp = ++liczbaPojazdow;
+        }
+
+        public Pojazd(string nazwa, byte liczbakol, double predkosc) : this()
+        {
+            try
+            {
+                this.Nazwa = nazwa;
+                this.LiczbaKol = liczbakol;
+                this.Predkosc = predkosc;
+            }
+            catch (ArgumentException)
+            {
+                this.Predkosc = 0.0;
+            }
+        }
+
+        public Pojazd(string nazwa, double predkosc) : this(nazwa, 4, predkosc)
+        { }
+
+    }
+
+    public class PojazdMechaniczny : Pojazd
+    {
+        private short moc_silnika;
+
+        public short MocSilnika
+        {
+            get
+            {
+                return moc_silnika;
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Nieprawidłowa wartość mocy");
+                else
+                    this.moc_silnika = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\t Moc: {this.MocSilnika}";
+        }
+
+        public PojazdMechaniczny(string nazwa, byte liczbakol, double predkosc, short moc_silnika) : base(nazwa, liczbakol, predkosc)
+        {
+            try
+            {
+                this.MocSilnika = moc_silnika;
+            }
+            catch (ArgumentException)
+            {
+                this.MocSilnika = 0;
+            }
+        }
+
+        public PojazdMechaniczny(string nazwa, double predkosc, short moc_silnika) : base(nazwa, predkosc)
+        {
+            try
+            {
+                this.MocSilnika = moc_silnika;
+            }
+            catch (ArgumentException)
+            {
+                this.MocSilnika = 0;
+            }
+        }
+
+    }
+
+    public class Samochod : PojazdMechaniczny
+    {
+        public string Marka { get; set; } = "";
+        public byte LiczbaPasazerow { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\t Marka: {this.Marka}\t L.Pas: {this.LiczbaPasazerow}";
+        }
+
+        public Samochod(string nazwa, byte liczbakol, double predkosc, short moc_silnika, string marka, byte liczba_pasazerow) : base(nazwa, liczbakol, predkosc, moc_silnika)
+        {
+            this.Marka = marka;
+            this.LiczbaPasazerow = liczba_pasazerow;
+        }
+
+        public Samochod(string nazwa, double predkosc, short moc_silnika, string marka, byte liczba_pasazerow) : base(nazwa, predkosc, moc_silnika)
+        {
+            this.Marka = marka;
+            this.LiczbaPasazerow = liczba_pasazerow;
+        }
+
+    }
+}
---
+++ C#/zad3-lab/zad3-lab/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Motoryzacja;
+
+namespace zad3_lab;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    void WyswietlPojazdy(IEnumerable<Pojazd> tab, ref ListBox lista)
+    {
+        lista.Items.Clear();
+
+        foreach (Pojazd p in tab)
+        {
+            ListBoxItem item = new();
+            item.Content = p;
+            lista.Items.Add(item);
+        }
+    }
+
+    void WyswietlHistorie(ref Pojazd p, ref ListBox lista)
+    {
+        lista.Items.Clear();
+    }
+
+    private void btnWypisz_Click(object sender, RoutedEventArgs e)
+    {
+        Pojazd[] tablica_pojazdow = new Pojazd[]
+        {
+            new("Mustang", 273.2),
+            new("Porsche", 264.6),
+            new("Rower", 2, 15.4),
+            new("Motocykl", 2, 184.2),
+            new(),
+            new PojazdMechaniczny("Kawasaki", 2, 323.3, 320),
+            new Samochod("Sportowy", 15.2, 35, "Maluch", 4)
+        };
+
+        WyswietlPojazdy(tablica_pojazdow, ref listPojazdy);
+    }
+}
